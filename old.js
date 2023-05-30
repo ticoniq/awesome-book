4cc60423b82fc500875f0945d28c3413a7b59cb3
hassanShakur <hass.shakurhassan@gmail.com>
2023-05-30 11:47:41 +0300

    Initialize code refadtoring to classes

--- old.js
+++ old.js
@@ -1,68 +1,47 @@
 const form = document.querySelector('.form');
 const msg = document.querySelector('.msg');
 const bookList = document.querySelector('.book-list');
-
 function Books(id, title, author) {
   this.id = id;
   this.title = title;
   this.author = author;
 }
 
-class UI {
-  // Create Display book on the UI
-  displayBooks(newBook) {
-    const li = document.createElement('li');
-    li.id = newBook.id;
-    li.innerHTML = `
+// Create Display book on the UI
+const displayBooks = (newBook) => {
+  const li = document.createElement('li');
+  li.id = newBook.id;
+  li.innerHTML = `
     <p>"${newBook.title}" by ${newBook.author}</p>
     <button id="remove" type="button">Remove</button>
   `;
-    bookList.appendChild(li);
+  bookList.appendChild(li);
+};
+
+const getBooks = () => {
+  let books;
+  if (localStorage.getItem('books') === null) {
+    books = [];
+  } else {
+    books = JSON.parse(localStorage.getItem('books'));
   }
 
-  // Display messages to the UI
-  getMessage(msg, msgClass) {
-    msg.innerText = 'Book removed successfully';
-    msg.classList.add('success');
-    setTimeout(() => {
-      msg.classList.remove('success');
-      msg.innerText = '';
-    }, 3000);
-  }
-}
+  return books;
+};
 
-class Store {
-  static getBooks() {
-    let books;
-    if (localStorage.getItem('books') === null) {
-      books = [];
-    } else {
-      books = JSON.parse(localStorage.getItem('books'));
-    }
+document.addEventListener('DOMContentLoaded', () => {
+  const books = getBooks();
 
-    return books;
-  }
+  books.forEach((book) => {
+    displayBooks(book);
+  });
+});
 
-  static addToLocalStorage(newBook) {
-    const books = Store.getBooks();
-    books.push(newBook);
-    localStorage.setItem('books', JSON.stringify(books));
-  }
-
-  static displayFromLocalStorage() {
-    const books = Store.getBooks();
-
-    const ui = new UI();
-    books.forEach((book) => {
-      ui.displayBooks(book);
-    });
-  }
-}
-
-document.addEventListener(
-  'DOMContentLoaded',
-  Store.displayFromLocalStorage
-);
+const addToLocalStorage = (newBook) => {
+  const books = getBooks();
+  books.push(newBook);
+  localStorage.setItem('books', JSON.stringify(books));
+};
 
 // Add books
 form.addEventListener('submit', (e) => {
@@ -83,13 +62,11 @@ form.addEventListener('submit', (e) => {
     // Init the books object
     const newBook = new Books(id, title, author);
 
-    const ui = new UI();
-
     // Display book on the UI
-    ui.displayBooks(newBook);
+    displayBooks(newBook);
 
     // Save books data to localStorage
-    Store.addToLocalStorage(newBook);
+    addToLocalStorage(newBook);
 
     msg.innerText = 'Book added successfully';
     msg.classList.add('success');
@@ -108,7 +85,7 @@ form.addEventListener('submit', (e) => {
 // Remove books
 bookList.addEventListener('click', (e) => {
   if (e.target.id === 'remove') {
-    const books = Store.getBooks();
+    const books = getBooks();
     const liParent = e.target.parentElement;
 
     const filteredBooks = books.filter(
